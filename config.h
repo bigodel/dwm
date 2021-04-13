796d058abf56ed20956c7aeb16b14f193840cf0b
João Pedro <jpedrodeamorim@gmail.com>
2021-04-13 09:28:49 -0300

    add firefox with lower case L

--- config.h
+++ config.h
@@ -29,6 +29,7 @@ static const Rule rules[] = {
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
 	{ "Firefox",  NULL,       NULL,       1 << 2,       0,           -1 }, /*3*/
+	{ "firefox",  NULL,       NULL,       1 << 2,       0,           -1 }, /*3*/
 	{ "Thunar",   NULL,       NULL,       0,            1,           -1 },
 	{ "Chromium-browser", NULL, NULL,     1 << 3,       0,           -1 }, /*4*/
 	{ "Chromium", NULL,       NULL,       1 << 3,       0,           -1 }, /*4*/
