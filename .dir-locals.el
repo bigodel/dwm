70a3dba433e7028bbe17bc57d3fab3c6cc4ece37
João Pedro <jpedrodeamorim@gmail.com>
2021-11-27 01:06:09 -0300

    iscentered config

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,1 +1,4 @@
-((nil . ((lsp-format-buffer-on-save . nil))))
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((c-mode . ((lsp-format-buffer-on-save . nil))))
